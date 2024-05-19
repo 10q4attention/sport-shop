11179ba19ab60a1ce3511a65447b5a487723a5b1
10q4attention <10q4attention@mail.ru>
2024-05-20 01:11:27 +0300

    added DeviseStorejs with few objects

---
+++ client/src/store/DeviceStore.js
@@ -0,0 +1,40 @@
+import {makeAutoObservable} from "mobx";
+
+export default class DeviceStore {
+    constructor() {
+        this._types = [
+            {id: 1, name: 'Холодильники'},
+            {id: 2, name: 'Смартфоны'},
+        ]
+        this._brands = [
+            {id: 1, name: 'LG'},
+            {id: 2, name: 'Apple'},
+        ]
+        this._devices = [
+            {id: 1, name: '12 pro', price: 1000, rating: 5, img: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSYWRcjtoYzJ7BuseTU81ERw2eUk2e1IC3p7jWmDaDEREu9AOkc82eIB27GqoCRRSX5I1LvnLUYHLuJiGcmQmAKc-sIu15dVb55dcS2k_iN&usqp=CAE'},
+            {id: 1, name: 'a51', price: 1000, rating: 5, img: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSYWRcjtoYzJ7BuseTU81ERw2eUk2e1IC3p7jWmDaDEREu9AOkc82eIB27GqoCRRSX5I1LvnLUYHLuJiGcmQmAKc-sIu15dVb55dcS2k_iN&usqp=CAE'},
+            {id: 1, name: 'notepro', price: 1000, rating: 5, img: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSYWRcjtoYzJ7BuseTU81ERw2eUk2e1IC3p7jWmDaDEREu9AOkc82eIB27GqoCRRSX5I1LvnLUYHLuJiGcmQmAKc-sIu15dVb55dcS2k_iN&usqp=CAE'},
+        ]
+        makeAutoObservable(this)
+    }
+
+    setTypes(types) {
+        this._types = types
+    }
+    setBrands(brands) {
+        this._brands = brands
+    }
+    setDevices(devices) {
+        this._devices = devices
+    }
+
+    get types() { //вызывается только, если переменная внутри была изменен
+        return this._types
+    }
+    get brands() { 
+        return this._brands
+    }
+    get devices() { 
+        return this._devices
+    }
+}
