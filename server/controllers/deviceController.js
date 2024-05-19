063798dedf7518d72cc5317e634ae24c5960c3d4
10q4attention <10q4attention@mail.ru>
2024-05-19 16:04:38 +0300

    working getOne fucntion in deviceController

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,6 +1,6 @@
 const uuid = require('uuid')
 const path = require('path')
-const {Device} = require('../models/models')
+const {Device, DeviceInfo} = require('../models/models')
 const ApiError = require('../error/ApiError')
 const { type } = require('os')
 
@@ -8,14 +8,24 @@ class DeviceController {
     async create(req, res, next) {
 
         try{
-            const {name, price, brandId, typeId, info} = req.body
+            let {name, price, brandId, typeId, info} = req.body
             const {img} = req.files
             let fileName = uuid.v4() + ".jpg"
             img.mv(path.resolve(__dirname, '..', 'static', fileName))
-
             const device = await Device.create({name, price, brandId, typeId, img: fileName})
 
-        return res.json(device)
+            if (info) {
+                info = JSON.parse(info)
+                IntersectionObserver.forEach(i =>
+                    DeviceInfo.create({
+                        title: i.title, 
+                        desctiption: i.desctiption,
+                        deviceId: device.id
+                    })
+                )
+            }
+
+            return res.json(device)
         } catch (e) {
             next(ApiError.badRequest(e.message))
         }
@@ -23,25 +33,37 @@ class DeviceController {
     }
 
     async getAll(req, res) {
-        const {brandId, typeId} = req.query
+        let {brandId, typeId, limit, page} = req.query
+        page = page || 1
+        limit = limit || 9
+        let offset = page * limit - limit
         let devices;
         if (!brandId && !typeId) {
-            devices = await Device.findAll()
+            devices = await Device.findAndCountAll({limit, offset})
         }
         if(brandId && !typeId){
-            devices = await Device.findAll({where:{brandId}})
+            devices = await Device.findAndCountAll({where:{brandId}, limit, offset})
         }
         if(!brandId && typeId){
-            devices = await Device.findAll({where:{typeId}})
+            devices = await Device.findAndCountAll({where:{typeId}, limit, offset})
         }
         if(brandId && typeId){
-            devices = await Device.findAll({where:{brandId}, typeId})
+            devices = await Device.findAndCountAll({where:{brandId, typeId}, limit, offset})
         }
         return res.json(devices)
     }
 
     async getOne(req, res) {
-        
+        const {id} = req.params
+        const device = await Device.findOne(
+            {
+                where: {id},
+                include: [{model: DeviceInfo, as: 'info'}]
+            },
+        )
+        return res.json(device)
     }
 }
 module.exports = new DeviceController()
+
+
