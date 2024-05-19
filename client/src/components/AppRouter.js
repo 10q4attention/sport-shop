b3091f235ab29849dc55ee94081c0a58aa7c4cd2
10q4attention <10q4attention@mail.ru>
2024-05-20 00:37:15 +0300

    added functions in UserStore.js

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,12 +1,24 @@
-import React from "react";
-import {Switch, Route, Redirect} from 'react-router-dom'
+import React, {useContext} from "react";
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { authRoutes, publicRoutes } from "../routes";
+import {Context} from "../index";
+
 
 const AppRouter = () => {
+    const {user} = useContext(Context)
+
+    console.log(user)
     return (
-        <div>
-            ADMIN PAGE
-        </div>
-    )
-}
+        <Routes>
+            {user.isAuth && authRoutes.map(({path, Component}) => 
+                <Route key={path} path={path} element={<Component />} />
+            )}
+            {publicRoutes && publicRoutes.map(({path, Component}) => 
+                <Route key={path} path={path} element={<Component />} />
+            )}
+            <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+    );
+};
 
 export default AppRouter;
